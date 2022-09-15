1f9b545f5eec59038d6137c78d77cda97a321289
George Pollard <porges@porg.es>
2022-09-15 00:32:49 +0000

    Update deps

--- lib/src/commands/list.rs
+++ lib/src/commands/list.rs
@@ -1,5 +1,6 @@
 use miette::IntoDiagnostic;
 use nu_table::StyledString;
+use terminal_size::{terminal_size, Height, Width};
 use tokio::io::AsyncWriteExt;
 
 pub struct List {
@@ -11,34 +12,45 @@ impl crate::commands::Command for List {
     async fn run<'c>(&self, ctx: crate::commands::Context<'c>) -> miette::Result<()> {
         let (_, config) = crate::nurfile::load_config(&ctx.cwd, &self.nur_file)?;
 
+        let text_style = Default::default();
         let taskdata: Vec<Vec<StyledString>> = config
             .tasks // already sorted by virtue of being in a BTreeMap
             .into_iter()
             .map(|(name, task)| {
                 vec![
-                    StyledString::new(name, Default::default()),
-                    StyledString::new(task.description, Default::default()),
+                    StyledString::new(name, text_style),
+                    StyledString::new(task.description, text_style),
                 ]
             })
             .collect();
 
         let table = nu_table::Table::new(
             vec![
-                StyledString::new("Name".to_string(), Default::default()),
-                StyledString::new("Description".to_string(), Default::default()),
+                StyledString::new("Name".to_string(), text_style),
+                StyledString::new("Description".to_string(), text_style),
             ],
             taskdata,
-            nu_table::Theme::rounded(),
+            nu_table::TableTheme::rounded(),
         );
+
+        let (Width(term_width), _) = terminal_size().unwrap_or((Width(80), Height(24)));
 
         let config = Default::default();
         let color_hm = Default::default();
-        let table = nu_table::draw_table(&table, 80, &color_hm, &config);
+        let alignments = Default::default();
+        if let Some(mut table) = table.draw_table(&config, &color_hm, alignments, term_width as usize) {
+            table.push('\n');
 
-        ctx.stdout
-            .write_all(table.as_bytes())
-            .await
-            .into_diagnostic()?;
+            ctx.stdout
+                .write_all(table.as_bytes())
+                .await
+                .into_diagnostic()?;
+        } else {
+            ctx.stderr
+                .write_all(b"Unable to fit table to terminal width")
+                .await
+                .into_diagnostic()?;
+        }
 
         Ok(())
     }
